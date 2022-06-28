807c3ae464a9d2c47a85609e13b8fa0f951938d4
Marcel Lieber <marcel.lieber@web.de>
2022-06-28 20:04:32 +0200

    Initial Commit

---
+++ Module 3 Lesson 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module 3 Lesson 2
+//
+//  Created by Marcel Lieber on 28.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module 3 Lesson 2/Module_3_Lesson_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Module_3_Lesson_2App.swift
+//  Module 3 Lesson 2
+//
+//  Created by Marcel Lieber on 28.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Module_3_Lesson_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
